ebc22368c8e791652adbcdd39ec77831e6ad71d0
koreaserver <102852097+koreaserver@users.noreply.github.com>
2022-04-16 00:14:08 +0900

    Add files via upload

---
+++ main/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .models import Post
+
+# Create your views here.
+def index(request):
+    return render(request,'main/index.html')
+
+def blog(request):
+    postlist = Post.objects.all()
+    return render(request, 'main/blog.html', {'postlist':postlist})
+
+def posting(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'main/posting.html', {'post':post})
+
+def new_post(request):
+    if request.method == 'POST':
+        if request.POST['mainphoto']:
+            new_article=Post.objects.create(
+                postname=request.POST['postname'],
+                contents=request.POST['contents'],
+                mainphoto=request.POST['mainphoto'],
+            )
+        else:
+            new_article=Post.objects.create(
+                postname=request.POST['postname'],
+                contents=request.POST['contents'],
+                mainphoto=request.POST['mainphoto'],
+            )
+        return redirect('/blog/')
+    return render(request, 'main/new_post.html')
